ca9e0e579cbe991ce4dbca9082ce8e6b8972cb60
Ahmed Kamel <ahmedkamel19992021@gmail.com>
2024-08-06 11:11:18 +0300

    loalStorage

--- JS/Index.js
+++ JS/Index.js
@@ -3,5 +3,14 @@ var sec = document.querySelector(".GetStart");
 
 picker.addEventListener(`input`, (e) => {
   var color = e.target.value;
-  sec.style.backgroundColor = color;
+  localStorage.setItem("color",color);
+  sec.style.backgroundColor = localStorage.getItem("color");
+  console.log(localStorage.getItem("color"));
+//   picker.onload(()=>{
+// e.style.backgroundColor=localStorage.getItem("color");
+//   })
 });
+document.addEventListener("DOMContentLoaded" , ()=>{
+sec.style.backgroundColor=localStorage.getItem("color") ;
+
+})
