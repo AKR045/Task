e3a14cce2bba869bc180dfeb2cec180abb431f8e
Ahmed Kamel <ahmedkamel19992021@gmail.com>
2024-08-06 20:57:42 +0300

    Map Done

--- JS/map.js
+++ JS/map.js
@@ -1,17 +1,32 @@
+var selected = document.querySelector("#select");
+selected.addEventListener("change", (e) => {
+  getPosition(e.target.value);
+});
 
+async function getPosition(x) {
+  console.log(x);
+  var result = await fetch(
+    `https://nominatim.openstreetmap.org/search?format=json&q=${x}`
+  );
+  var data = await result.json();
+  var lng = Number(data[0].lon);
+  var lat = Number(data[0].lat);
+  console.log(typeof lat);
+  initMap(lat, lng);
+}
 
-function initMap() {
-    const myLatLng = { lat: -25.363, lng: 131.044 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: myLatLng,
-    });
-  
-    new google.maps.Marker({
-      position: myLatLng,
-      map,
-      title: "Hello World!",
-    });
-  }
-  
-  window.initMap = initMap;
+function initMap(myLat = 26.2540493, myLang = 29.2675469) {
+  const myLatLng = { lat: myLat, lng: myLang };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatLng,
+  });
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
+  });
+}
+
+window.initMap = initMap;
